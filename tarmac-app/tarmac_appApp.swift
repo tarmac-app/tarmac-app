c6346112fe3abe47a008ce63c8866e5321d02328
jiangoliver <47641674+jiangoliver@users.noreply.github.com>
2020-12-02 16:08:33 -0500

    Initial Commit

---
+++ tarmac-app/tarmac_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tarmac_appApp.swift
+//  tarmac-app
+//
+//  Created by Oliver Jiang on 12/2/20.
+//
+
+import SwiftUI
+
+@main
+struct tarmac_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
